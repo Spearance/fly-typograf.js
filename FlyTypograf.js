e4f6bcca00c6ab356f27b71eb8545ab680f189ca
Evgeniy Lepeshkin <spearance@gmail.com>
2021-12-19 18:09:27 +0500

    major changes

--- FlyTypograf.js
+++ FlyTypograf.js
@@ -1,106 +1,272 @@
 /*
-FlyTypograf.js
+	FlyTypograf.js
 
 	https://github.com/Spearance/FlyTypograf.js
-	http://www.typograf.ru/
+	https://typograf.ru
 
 	Copyright 2021, Evgeniy Lepeshkin
 
 	Released under the MIT license.
 	http://www.opensource.org/licenses/mit-license.php
 
-	Version: v 0.1
+	Version: v 1.0
 	Date: Aug 26, 2021
  */
 
 export class FlyTypograf {
 	#original = ``
 	#result = ``
+	#caretPosition = 0
+
+	#leftQuote = `«`
+	#rightQuote = `»`
+
+	#decimals = {
+		"1/2": `½`,
+		"1/3": `⅓`,
+		"1/4": `¼`,
+		"1/5": `⅕`,
+		"1/6": `⅙`,
+		"1/8": `⅛`,
+		"2/3": `⅔`,
+		"2/5": `⅖`,
+		"3/4": `¾`,
+		"3/5": `⅗`,
+		"3/8": `⅜`,
+		"4/5": `⅘`,
+		"5/6": `⅚`,
+		"5/8": `⅝`,
+		"7/8": `⅞`
+	}
+
+	#rules = [
+		{
+			// Minus sign
+			pattern: / -(\d)/g,
+			replace: ` −$1`
+		},
+		{
+			// Dash sign
+			pattern: /(^|\n|\s|>)\-(\s)/g,
+			replace: `$1—$2`
+		},
+		{
+			// Double hyphen
+			pattern: /(?<![-!])-{2} /g,
+			replace: () => {
+				this.#caretPosition--
+				return `— `
+			}
+		},
+		{
+			// Multiple spaces
+			pattern: /\u00a0{2,}|\u00a0 | \u00a0/g, //здесь неправильно, надо давать вводить два пробела после привязанных союзов
+			replace: (str) => {
+				this.#caretPosition -= str.length - 1
+				return `\u00a0`
+			}
+		},
+		{
+			// Numerical interval
+			pattern: /(\d)\s?[-—]\s?(\d)/g,
+			replace: (str, $1, $2) => {
+				this.#caretPosition -= str.length - `${$1}–${$2}`.length
+				return `${$1}–${$2}`
+			}
+		},
+		{
+			// Copyright (c)
+			pattern: /\([cс]\)/ig,
+			replace: () => {
+				this.#caretPosition -= 2
+				return `©`
+			}
+		},
+		{
+			// Registered trademark (R)
+			pattern: /\(r\)/ig,
+			replace: () => {
+				this.#caretPosition -= 2
+				return `®`
+			}
+		},
+		{
+			// Trademark (TM)
+			pattern: /\(tm\)/ig,
+			replace: () => {
+				this.#caretPosition -= 3
+				return `™`
+			}
+		},
+		{
+			// Rouble (р)
+			pattern: /\(р\)/ig,
+			replace: () => {
+				this.#caretPosition -= 2
+				return `₽`
+			}
+		},
+		{
+			// Three dots
+			pattern: /(?<![.…])\.{3}/g,
+			replace: () => {
+				this.#caretPosition -= 2
+				return `…`
+			}
+		},
+		{
+			// Fix more then three dots
+			pattern: /…(\.+)/g,
+			replace: (str, $1) => {
+				this.#caretPosition += 2
+				return `...${$1}`
+			}
+		},
+		{
+			// Sizes
+			pattern: /(\d)[xх](\d)/ig,
+			replace: `$1×$2`
+		},
+		{
+			// Plus/Minus +/-
+			pattern: /\+\/\-/g,
+			replace: () => {
+				this.#caretPosition -= 2
+				return `±`
+			}
+		},
+		{
+			// Decimals like 1/2
+			pattern: /\b([123457]\/[234568])\b/g,
+			replace: (str, $1) => {
+				if (this.#decimals[`${$1}`]) {
+					this.#caretPosition -= 2
+				}
+				return this.#decimals[`${$1}`] ? this.#decimals[`${$1}`] : `${$1}`
+			}
+		},
+		{
+			// Fix decimals with next number
+			pattern: new RegExp(`([${Object.values(this.#decimals).join('')}])(\\d)`, `g`),
+			replace: (str, $1, $2) => {
+				this.#caretPosition += 2
+				return `${Object.entries(this.#decimals).find(i => i[1] === $1)[0]}${$2}`
+			}
+		},
+		{
+			// Open quote
+			pattern: /["»]([a-z0-9а-яё…])/ig,
+			replace: `${this.#leftQuote}$1`
+		},
+		{
+			// Close quote
+			pattern: /([a-z0-9а-яё…?!])["«]/ig,
+			replace: `$1${this.#rightQuote}`
+		},
+		{
+			// Open quote
+			pattern: new RegExp(`"(${this.#leftQuote}[a-zа-яё0-9…])`, `ig`),
+			replace: `${this.#leftQuote}$1`
+		},
+		{
+			// Close quote
+			pattern: new RegExp(`([a-zа-яё0-9…?!]${this.#rightQuote})"`, `ig`),
+			replace: `$1${this.#rightQuote}`
+		},
+		{
+			// Fix HTML open quotes
+			pattern: new RegExp(`([-a-z0-9]+=)[${this.#leftQuote}${this.#rightQuote}]([^${this.#leftQuote}${this.#rightQuote}]*?)`, `ig`),
+			replace: `$1"$2`
+		},
+		{
+			// Fix HTML close quotes
+			pattern: new RegExp(`([-a-z0-9]+=)[\"]([^>${this.#leftQuote}${this.#rightQuote}]*?)[${this.#leftQuote}${this.#rightQuote}]`, `ig`),
+			replace: `$1"$2"`
+		},
+		{
+			// English apos
+			pattern: /\b'([std]|ve|ll|clock)\b/ig,
+			replace: `’$1`
+		},
+		{
+			// Minutes and seconds
+			pattern: new RegExp(`([0-6]?[0-9])[\'\′]([0-6]?[0-9])?(\\d+)[${this.#rightQuote}\"]`, `g`),
+			replace: `$1′$2$3″`
+		},
+		{
+			// Prepositions
+			pattern: /((?:^|\n|\t|[\u00a0 ]|>)[a-zа-яё]{1,2}) /ig,
+			replace: `$1\u00a0`
+		},
+		{
+			// Particles with dash and non-breaking space
+			pattern: /\-(то|ка)\u00a0/gi,
+			replace: `-$1 `
+		},
+		{
+			// Particles
+			pattern: /(?:\s|\t|[\u00a0 ])(же?|л[иь]|бы?|ка)([.,!?:;])?\u00a0/ig,
+			replace: `\u00a0$1$2 `
+		}
+	];
 
 	constructor (textElement) {
 		this._element = textElement
-
-		this._leftQuote = `«`
-		this._rightQuote = `»`
+		this._isContentEditable = this._element.contentEditable === true
 	}
 
 	get result() {
 		return this.#result
 	}
 
-	get original() {
-		return this.#original
-	}
-
-	set original(text) {
-		this.#original = text;
-	}
-
 	process () {
-		this.#original = this._element.value
+		this.#result = this._element.value
 
-		this.#result = this.#original
-			// Minus
-			.replace(/\u0020-(\d)/g, `\u0020−$1`)
+		this._getCaretPosition()
 
-			// Dash
-			.replace(/(^|\n|\s|>)\-(\s)/g, `$1—$2`)
+		this.#rules.forEach((regex) => {
+			this.#result = this.#result.replace(regex.pattern, regex.replace)
+		})
 
-			// Double hyphen
-			.replace(/(?<![-!])-{2} /g, `— `)
+		this._element.value = this.#result
 
-			// Multiple nbsp
-			.replace(/\u00a0{2,}|\u00a0\u0020|\u0020\u00a0/g, `\u00a0`)
+		this._setCaretPosition(this.#caretPosition)
+	}
 
-			// Numerical interval
-			.replace(/(\d)(?:\u0020)?[-—](?:\u0020)?(\d)/g, (str, $1, $2) => `${$1}–${$2}`)
+	_getSelectionText () {
+		if (window.getSelection) {
+			return window.getSelection()
+		}
 
-			// Copyright
-			.replace(/\([cс]\)/ig, `©`)
+		if (document.getSelection) {
+			return document.getSelection()
+		}
 
-			// Registered trademark
-			.replace(/\(r\)/ig, `®`)
+		if (document.selection) {
+			return document.selection.createRange().text
+		}
+	}
 
-			// Trademark
-			.replace(/\(tm\)/ig, `™`)
+	_getCaretPosition () {
+		if (this._isContentEditable) {
+			this._element.focus()
+			let _range = document.getSelection().getRangeAt(0)
+			let range = _range.cloneRange()
+			range.selectNodeContents(this._element)
+			range.setEnd(_rang.endContainer, _range.endOffset)
+			return range.toString().length
+		}
 
-			// Rouble
-			.replace(/\([рp]\)/ig, `₽`)
+		this.#caretPosition = this._element.selectionStart
+	}
 
-			// Three dots
-			.replace(/\.{3}/g, `…`)
+	_setCaretPosition (pos) {
+		if (this._isContentEditable) {
+			this._element.focus()
+			document.getSelection().collapse(this._element, pos)
+			return
+		}
 
-			// Sizes
-			.replace(/(\d)[xх](\d)/ig, `$1×$2`)
-
-			// Open quote
-			.replace(/\"([a-z0-9\u0410-\u042f\u0401…])/ig, `${this._leftQuote}$1`)
-
-			// Close quote
-			.replace(/([a-z0-9\u0410-\u042f\u0401…?!])\"/ig, `$1${this._rightQuote}`)
-
-			// Open quote
-			.replace(new RegExp(`\"(${this._leftQuote}[a-zа-яё0-9…])`, `ig`), `${this._leftQuote}$1`)
-
-			// Close quote
-			.replace(new RegExp(`([a-zа-яё0-9…?!]${this._rightQuote})\"`, `ig`), `$1${this._rightQuote}`)
-
-			// Fix HTML open quotes
-			.replace(new RegExp(`([-a-z0-9]+=)[${this._leftQuote}${this._rightQuote}]([^${this._leftQuote}${this._rightQuote}]*?)`, `ig`), `$1\"$2`)
-
-			// Fix HTML close quotes
-			.replace(new RegExp(`([-a-z0-9]+=)[\"]([^>${this._leftQuote}${this._rightQuote}]*?)[${this._leftQuote}${this._rightQuote}]`, `ig`), `$1\"$2\"`)
-
-			// Minutes and seconds
-			.replace(new RegExp(`([0-6]?[0-9])[\'\′]([0-6]?[0-9])?(\\d+)[${this._rightQuote}\"]`, `g`), `$1\′$2$3\″`)
-
-			// Prepositions
-			.replace(/((?:^|\n|\t|[\u00a0 ]|>)[a-zа-яё]{1,2}) /ig, `$1\u00a0`)
-
-			.replace(/\-(то|ка)\u00a0/gi, `-$1\u0020`)
-
-			.replace(/(?:\s|\t|[\u00a0 ])(же?|л[иь]|бы?|ка)([.,!?:;])?\u00a0/ig, `\u00a0$1$2\u0020`);
-
-			this._element.value = this.#result;
+		this._element.setSelectionRange(pos, pos)
 	}
 }
--- example/example.js
+++ example/example.js
@@ -2,19 +2,20 @@ import {FlyTypograf} from '../FlyTypograf.js';
 
 const textarea = document.querySelector(`#typograf`)
 const highLight = document.querySelector(`#highLight`)
-const Typograf = new FlyTypograf(textarea)
 
-// Highlight changed text
+// Highlight changed text for demo only
 textarea.addEventListener(`keyup`, () => {
   highLight.innerHTML = `<p>${textarea.value
     .replace(/\u00a0/g, `<span class="hlg">&nbsp\;</span>`)
 
-    .replace(/([…©®™₽×—–„“«»″′])/g, `<span class="hlb">$1</span>`)
+    .replace(/([…©®™₽×—–„“«»″′½⅓¼⅕⅙⅛⅔⅖¾⅗⅜⅘⅚⅝⅞])/g, `<span class="hlb">$1</span>`)
 
     .replace(/\n{2,}/g, "</p><p>")
 
     .replace(/\n/g, "<br />")}</p>`
 });
+
+const Typograf = new FlyTypograf(textarea)
 
 const onTextAreaInput = () => {
   Typograf.process()
