645fb4171e262a49e2f2e07ca31777803414729c
Evgeniy Lepeshkin <spearance@gmail.com>
2021-12-27 12:52:20 +0500

    добавил минус в подсветку

--- example/example.js
+++ example/example.js
@@ -8,7 +8,7 @@ textarea.addEventListener(`keyup`, () => {
   highLight.innerHTML = `<p>${textarea.value
     .replace(/\u00a0/g, `<span class="hlg">&nbsp\;</span>`)
 
-    .replace(/([…©®™₽×—–„“«»″′’½⅓¼⅕⅙⅛⅔⅖¾⅗⅜⅘⅚⅝⅞±])/g, `<span class="hlb">$1</span>`)
+    .replace(/([…©®™₽×—–−„“«»″′’½⅓¼⅕⅙⅛⅔⅖¾⅗⅜⅘⅚⅝⅞±])/g, `<span class="hlb">$1</span>`)
 
     .replace(/\n{2,}/g, "</p><p>")
 
